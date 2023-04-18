2f380f63b44a1261e9a8b2d005d5544b59e15198
galipo <black0velta@gmail.com>
2023-04-18 02:41:05 +0100

    first mern app

--- src/utils/sendData.js
+++ src/utils/sendData.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 const baseURL = `http://localhost:5000/api/v1/tasks`
 const sendData = (data, Settext, fetchingData, setTODO) => {
+  if (data.trim().length === 0) {
+    return
+  }
   axios.post(baseURL, { name: data }).then(() => {
     Settext('')
     fetchingData(setTODO)
